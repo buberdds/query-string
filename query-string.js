6ca6c5a84002774f6a60f2df52ff321eade12f19
Michal Zielenkiewicz <michal.zielenkiewicz@allegro.pl>
2014-04-22 23:01:24 +0200

    IE8 support

--- query-string.js
+++ query-string.js
@@ -14,6 +14,47 @@
 			return {};
 		}
 
+    if(typeof String.prototype.trim !== 'function') {
+      String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+      }
+    }
+
+    if ('function' !== typeof Array.prototype.reduce) {
+      Array.prototype.reduce = function(callback, opt_initialValue){
+        'use strict';
+        if (null === this || 'undefined' === typeof this) {
+          throw new TypeError(
+              'Array.prototype.reduce called on null or undefined');
+        }
+        if ('function' !== typeof callback) {
+          throw new TypeError(callback + ' is not a function');
+        }
+        var index, value,
+            length = this.length >>> 0,
+            isValueSet = false;
+        if (1 < arguments.length) {
+          value = opt_initialValue;
+          isValueSet = true;
+        }
+        for (index = 0; length > index; ++index) {
+          if (this.hasOwnProperty(index)) {
+            if (isValueSet) {
+              value = callback(value, this[index], index, this);
+            }
+            else {
+              value = this[index];
+              isValueSet = true;
+            }
+          }
+        }
+        if (!isValueSet) {
+          throw new TypeError('Reduce of empty array with no initial value');
+        }
+        return value;
+      };
+    }
+
 		str = str.trim().replace(/^\?/, '');
 
 		if (!str) {
