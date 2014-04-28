3a8c70b943c5f543f001e14174f022ed5e2cedda
Michal Zielenkiewicz <michal.zielenkiewicz@allegro.pl>
2014-04-28 13:39:55 +0200

    underscore dep

--- query-string.js
+++ query-string.js
@@ -1,18 +1,19 @@
 /*!
-	query-string
-	Parse and stringify URL query strings
-	https://github.com/sindresorhus/query-string
-	by Sindre Sorhus
-	MIT License
-*/
+ query-string
+ Parse and stringify URL query strings
+ https://github.com/sindresorhus/query-string
+ by Sindre Sorhus
+ MIT License
+ */
 (function () {
-	'use strict';
-	var queryString = {};
+  'use strict';
+  var queryString = {},
+      argsArray;
 
-	queryString.parse = function (str) {
-		if (typeof str !== 'string') {
-			return {};
-		}
+  queryString.parse = function (str) {
+    if (typeof str !== 'string') {
+      return {};
+    }
 
     if(typeof String.prototype.trim !== 'function') {
       String.prototype.trim = function() {
@@ -20,88 +21,55 @@
       }
     }
 
-    if ('function' !== typeof Array.prototype.reduce) {
-      Array.prototype.reduce = function(callback, opt_initialValue){
-        'use strict';
-        if (null === this || 'undefined' === typeof this) {
-          throw new TypeError(
-              'Array.prototype.reduce called on null or undefined');
-        }
-        if ('function' !== typeof callback) {
-          throw new TypeError(callback + ' is not a function');
-        }
-        var index, value,
-            length = this.length >>> 0,
-            isValueSet = false;
-        if (1 < arguments.length) {
-          value = opt_initialValue;
-          isValueSet = true;
-        }
-        for (index = 0; length > index; ++index) {
-          if (this.hasOwnProperty(index)) {
-            if (isValueSet) {
-              value = callback(value, this[index], index, this);
-            }
-            else {
-              value = this[index];
-              isValueSet = true;
-            }
-          }
-        }
-        if (!isValueSet) {
-          throw new TypeError('Reduce of empty array with no initial value');
-        }
-        return value;
-      };
+    str = str.trim().replace(/^\?/, '');
+
+    if (!str) {
+      return {};
     }
 
-		str = str.trim().replace(/^\?/, '');
+    argsArray = str.trim().split('&');
 
-		if (!str) {
-			return {};
-		}
+    return _(argsArray).reduce(function (ret, param) {
+      var parts = param.replace(/\+/g, ' ').split('=');
+      var key = parts[0];
+      var val = parts[1];
 
-		return str.trim().split('&').reduce(function (ret, param) {
-			var parts = param.replace(/\+/g, ' ').split('=');
-			var key = parts[0];
-			var val = parts[1];
+      key = decodeURIComponent(key);
+      // missing `=` should be `null`:
+      // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+      val = val === undefined ? null : decodeURIComponent(val);
 
-			key = decodeURIComponent(key);
-			// missing `=` should be `null`:
-			// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
-			val = val === undefined ? null : decodeURIComponent(val);
+      if (!ret.hasOwnProperty(key)) {
+        ret[key] = val;
+      } else if (_.isArray(ret[key])) {
+        ret[key].push(val);
+      } else {
+        ret[key] = [ret[key], val];
+      }
 
-			if (!ret.hasOwnProperty(key)) {
-				ret[key] = val;
-			} else if (Array.isArray(ret[key])) {
-				ret[key].push(val);
-			} else {
-				ret[key] = [ret[key], val];
-			}
+      return ret;
+    }, {});
+  };
 
-			return ret;
-		}, {});
-	};
+  queryString.stringify = function (obj) {
+    return obj ? _.chain(obj).keys(obj).map(function (key) {
+      var val = obj[key];
 
-	queryString.stringify = function (obj) {
-		return obj ? Object.keys(obj).map(function (key) {
-			var val = obj[key];
+      if (_.isArray(val)) {
+        return _(val).map(function (val2) {
+          return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
+        }).join('&');
+      }
 
-			if (Array.isArray(val)) {
-				return val.map(function (val2) {
-					return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
-				}).join('&');
-			}
+      return encodeURIComponent(key) + '=' + encodeURIComponent(val);
+    }).join('&').value() : '';
+  };
 
-			return encodeURIComponent(key) + '=' + encodeURIComponent(val);
-		}).join('&') : '';
-	};
-
-	if (typeof define === 'function' && define.amd) {
-		define([], queryString);
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = queryString;
-	} else {
-		window.queryString = queryString;
-	}
+  if (typeof define === 'function' && define.amd) {
+    define([], queryString);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = queryString;
+  } else {
+    window.queryString = queryString;
+  }
 })();
